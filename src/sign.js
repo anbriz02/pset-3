bff60ba59d360699313a1d4a0a2b442d96ea802b
Andy Lopez <ALopez@ucvts.tec.nj.us>
2019-10-30 10:49:26 -0400

    sdfvbjfgbuyhd

--- src/sign.js
+++ src/sign.js
@@ -2,10 +2,17 @@ const readlineSync = require("readline-sync");
 
 const amount = Number(readlineSync.question("\nEnter a number: "));
 
-if () {
-    console.log("\n\n")
-} else if () {
-    console.log("\n\n")
-} else {
-    console.log("\n"")
+const MAX = Number.MAX_SAFE_INTEGER;
+const MIN = Number.MIN_SAFE_INTEGER;
+
+if (Number.isNaN(amount)) {
+    console.log("\nInvalid")
+} else if (amount < MIN || amount > MAX) {
+    console.log("\nInvalid")
+} else if (amount === 0) {
+    console.log("\nZero.")
+} else if (amount > 0) {
+    console.log("\nPositive.")
+} else if (amount < 0) {
+    console.log("\nNegative.")
 }
